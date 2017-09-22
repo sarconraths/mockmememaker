0ad02fc146a5db1fbc58fecf49e8848b873ac08b
sarconraths <30857692+sarconraths@users.noreply.github.com>
2017-09-22 18:47:17 +0200

    Added 0.2 feature wishes, corrected uppercase comp
    mock should now work with all uppercase strings as input

--- mockscript.js
+++ mockscript.js
@@ -46,7 +46,9 @@ function randomizeBottom(){
 	var newstring = '';
 		for(var i = 0; i < torandom.length; i++){
 			if(Math.random()> 0.5){
-				newstring += torandom.charAt(i); 
+				var a = torandom.charAt(i);
+				var b = a.toLowerCase();
+				newstring += b; 
 			}else{
 				var x = torandom.charAt(i);
 				var y = x.toUpperCase(); 
@@ -63,5 +65,17 @@ function disableStart() {
 	document.getElementById("start").removeEventListener("click", randomizeTop);
 	document.getElementById("start").removeEventListener("click", randomizeBottom);
 }
+
+
+0.2:
+
+Reactivate start Button on change of input fields:
+ - user puts in text, clicks mock me. changes text - mock me won't work. This needs fixing for sure. 
+ - also if he decides to add a top line, mock me must work. Mock me must work whenever something in the input area is changed
+Make "shuffle" work only for the text that has been sent through the "mock me" button
+ - possibly create variable that is independent from the onclick event?
+ - so: User input, "mock me" - text appears in picture. User changes input, decides he wants to keep the original - but if he presses shuffle, 
+	 the new input will be used. Instead I want to use what is already there
+Make mock work for uppercase strings (add a "if...-> char uppercase", "else...-> char lowercase"), this should be simple enough
 
 
